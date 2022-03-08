23327cacc19d32998580d522cc30763f4f626905
Shaurya <shaurya@Administrators-MacBook-Air.local>
2022-03-08 11:23:26 +0530

    fix: remove default from `file` option

--- src/utils/cmdArgs.js
+++ src/utils/cmdArgs.js
@@ -4,7 +4,6 @@ exports.fetchCliArgs = () => {
     program.option(
         '-f, --file <value>',
         'Path to postman collection file',
-        '/Users/shaurya/Documents/testing/Node-Automate/Odin.postman_collection.json'
     )
     program.parse()
     return program.opts()
